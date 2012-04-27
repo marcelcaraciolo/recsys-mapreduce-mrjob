f358ad96635b2685de0118274858e337c8deafc4
Marcel Caraciolo <caraciol@gmail.com>
2012-04-27 19:03:39 -0300

    New version of moviesSimilarities

---
+++ metrics.py
@@ -0,0 +1,55 @@
+#-*-coding: utf-8 -*-
+
+'''
+Similarities measures
+
+'''
+
+__author__ = 'Marcel Caraciolo <caraciol@gmail.com>'
+
+
+from math import sqrt
+
+
+def correlation(size, dot_product, rating_sum, \
+            rating2sum, rating_norm_squared, rating2_norm_squared):
+    '''
+      The correlation between two vectors A, B is
+      cov(A, B) / (stdDev(A) * stdDev(B))
+
+    '''
+    numerator = size * dot_product - rating_sum * rating2sum
+    denominator = sqrt(size * rating_norm_squared - rating_sum * rating_sum) * \
+                    sqrt(size * rating2_norm_squared - rating2sum * rating2sum)
+
+    return numerator / float(denominator)
+
+
+def combinations(iterable, r):
+    """
+    Implementation of itertools combinations method. Re-implemented here because
+    of import issues in Amazon Elastic MapReduce. Was just easier to do this than
+    bootstrap.
+    More info here: http://docs.python.org/library/itertools.html#itertools.combinations
+
+    Input/Output:
+
+    combinations('ABCD', 2) --> AB AC AD BC BD CD
+    combinations(range(4), 3) --> 012 013 023 123
+    """
+    pool = tuple(iterable)
+    n = len(pool)
+    if r > n:
+        return
+    indices = range(r)
+    yield tuple(pool[i] for i in indices)
+    while True:
+        for i in reversed(range(r)):
+            if indices[i] != i + n - r:
+                break
+        else:
+            return
+        indices[i] += 1
+        for j in range(i + 1, r):
+            indices[j] = indices[j - 1] + 1
+        yield tuple(pool[i] for i in indices)
--- moviesSimilarities.py
+++ moviesSimilarities.py
@@ -11,25 +11,68 @@
 
 
 '''
-
 __author__ = 'Marcel Caraciolo <caraciol@gmail.com>'
 
 from mrjob.job import MRJob
+from metrics import correlation
+try:
+    from itertools import combinations
+except ImportError:
+    from metrics import combinations
 
 
 class MoviesSimilarities(MRJob):
 
-	def mapper(self, key, line):
-		"""
-		Mapper: send score from a single movie to
-		other movies
-		"""
-		for user_id, item_id, rating in line.split(';'):
-			print user_id, item_id, rating
-		
-	def reducer(self, _, values):
-		pass
+    def steps(self):
+        return [self.mr(self.group_by_user_rating,
+                         self.count_ratings_users_freq),
+                self.mr(self.pairwise_items,
+                    self.calculate_similarity)
+                ]
 
+    def group_by_user_rating(self, key, line):
+        """
+        Mapper: send score from a single movie to
+        other movies
+        """
+        user_id, item_id, rating = line.split('|')
+        #yield (item_id, int(rating)), user_id
+        #yield item_id, (user_id, int(rating))
+        yield  user_id, (item_id, int(rating))
+        #yield (user_id, item_id), int(rating)
+
+    def count_ratings_users_freq(self, user_id, values):
+        item_count = 0
+        item_sum = 0
+        final = []
+        for item_id, rating in values:
+            item_count += 1
+            item_sum += rating
+            final.append((item_id, rating))
+
+        yield user_id, (item_count, item_sum, final)
+
+    def pairwise_items(self, user_id, values):
+        item_count, item_sum, ratings = values
+        #print item_count, item_sum, [r for r in combinations(ratings, 2)]
+        #bottleneck at combinations
+        for item1, item2 in combinations(ratings, 2):
+            yield (item1[0], item2[0]), \
+                    (item1[1], item2[1])
+
+    def calculate_similarity(self, pair_key, lines):
+        sum_xx, sum_xy, sum_yy, sum_x, sum_y, n = (0.0, 0.0, 0.0, 0.0, 0.0, 0)
+        item_pair, co_ratings = pair_key, lines
+        item_xname, item_yname = item_pair
+        for item_x, item_y in lines:
+            sum_xx += item_x * item_x
+            sum_yy += item_y * item_y
+            sum_xy += item_x * item_y
+            sum_y += item_y
+            sum_x += item_x
+            n += 1
+        similarity = correlation(n, sum_xy, sum_x, sum_y, sum_xx, sum_yy)
+        yield (item_x, item_y), (similarity, n)
 
 if __name__ == '__main__':
     MoviesSimilarities.run()
